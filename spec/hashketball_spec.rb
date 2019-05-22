276365f9d363b637522eb8439a8ac67accc0b854
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 23:51:36 +0000

    Automatically backed up by Learn

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -266,15 +266,15 @@ describe 'bonus' do
 
   end
 
-#   describe '#player_with_longest_name' do
+  describe '#player_with_longest_name' do
 
-#     it 'returns Brendan Haywood' do
-#       expect(player_with_longest_name).to eq("Brendan Haywood")
-#     end
+    it 'returns Brendan Haywood' do
+      expect(player_with_longest_name).to eq("Brendan Haywood")
+    end
 
-#   end
+  end
 
-# end
+end
 
 # describe 'super bonus' do
 
