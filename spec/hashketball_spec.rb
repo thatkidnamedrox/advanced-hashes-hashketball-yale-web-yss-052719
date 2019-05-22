5ff553faefc2530def9c762ab4a5349c732e1cd0
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 23:40:12 +0000

    Automatically backed up by Learn

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -248,15 +248,15 @@ end
 
 # If you'd like to work on the bonus, uncomment these tests.
 
-# describe 'bonus' do
+describe 'bonus' do
 
-#   describe '#most_points_scored' do
+  describe '#most_points_scored' do
 
-#     it 'returns Ben Gordon' do
-#       expect(most_points_scored).to eq("Ben Gordon")
-#     end
+    it 'returns Ben Gordon' do
+      expect(most_points_scored).to eq("Ben Gordon")
+    end
 
-#   end
+  end
 
 #   describe '#winning_team' do
 
